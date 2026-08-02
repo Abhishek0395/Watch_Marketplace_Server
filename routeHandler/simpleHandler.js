91ac2c14eee2b6ffa1e8fe4ca7a847688dada91b
Abhishek0395 <abhishek.agarwal0395@gmail.com>
2026-08-02 15:37:29 +0600

    simple fix

--- routeHandler/simpleHandler.js
+++ routeHandler/simpleHandler.js
@@ -2,30 +2,14 @@ const express = require('express');
 const Watch = require('../schemas/watchSchema');
 
 const router = express.Router();
-// router.get('/', async (req, res) => {
-//     try {
-//         const watches = await Watch.find({ status: 'approved' });
-//         res.status(200).json(watches);
-//     } catch (err) {
-//         res.status(500).json({
-//             error: err.message,
-//         });
-//     }
-// });
 router.get('/', async (req, res) => {
     try {
-        const dbName = Watch.db.name;
-        const total = await Watch.countDocuments({});
-        const statuses = await Watch.distinct('status'); // <-- every distinct status value
-        const sample = await Watch.findOne({}, 'title status'); // <-- one doc's title + status
-        res.status(200).json({
-            dbName,
-            total,
-            statuses,
-            sample,
-        });
+        const watches = await Watch.find({ status: 'approved' });
+        res.status(200).json(watches);
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({
+            error: err.message,
+        });
     }
 });
 module.exports = router;
